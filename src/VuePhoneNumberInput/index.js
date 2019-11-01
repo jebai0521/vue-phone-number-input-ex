1926c84793775729df140d6cca628266d31d08da
Chen Ming <mingchen@waveo.com>
2019-11-01 17:30:14 +0800

    update

---
+++ src/VuePhoneNumberInput/index.js
@@ -0,0 +1,28 @@
+
+import VuePhoneNumberInput from './index.vue'
+
+export function install(Vue) {
+  if (install.installed) return
+  install.installed = true
+
+  Vue.component('vue-phone-number-input', VuePhoneNumberInput)
+}
+
+export { VuePhoneNumberInput }
+
+const plugin = {
+  install,
+}
+
+// Auto-install
+let GlobalVue = null
+if (typeof window !== 'undefined') {
+  GlobalVue = window.Vue
+} else if (typeof global !== 'undefined') {
+  GlobalVue = global.Vue
+}
+if (GlobalVue) {
+  GlobalVue.use(plugin)
+}
+
+export default plugin
